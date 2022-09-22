d88a613795f4fbd51bbbe1c852cf39e6e3b56b84
Nicholas Tietz-Sokolsky <me@ntietz.com>
2022-09-22 09:05:32 -0400

    Implement responses as always errors

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -1,15 +1,15 @@
-use tokio::net::TcpStream;
-use tokio::io::{AsyncReadExt};
-use std::net::SocketAddr;
-use std::io::Cursor;
 use bytes::{Buf, BytesMut};
+use std::io::Cursor;
+use std::net::SocketAddr;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
-use crate::codec::decode;
+use crate::codec::{decode, encode, Atom};
 
 pub async fn accept_connection(mut socket: TcpStream, addr: SocketAddr) -> std::io::Result<()> {
     log::info!("Accepting connection from {}", addr);
 
-    let mut buffer = BytesMut::with_capacity(4*1024);
+    let mut buffer = BytesMut::with_capacity(4 * 1024);
 
     loop {
         if 0 == socket.read_buf(&mut buffer).await? {
@@ -23,6 +23,14 @@ pub async fn accept_connection(mut socket: TcpStream, addr: SocketAddr) -> std::
                 let pos = cursor.position() as usize;
                 println!("pos: {}, token: {:?}, buf: {:?}", pos, token, buffer);
                 buffer.advance(pos);
+
+                if buffer.is_empty() {
+                    let resp = Atom::Error("Not implemented yet".to_string());
+                    let mut write_buf: Vec<u8> = vec![];
+                    encode(&mut write_buf, &resp); // TODO: handle error
+
+                    socket.write_all(&write_buf).await?;
+                }
             } else {
                 break;
             }
