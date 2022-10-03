16f93f506b0a1a194567d7199a81540275ae8d30
Miccah Castorina <m.castorina93@gmail.com>
2022-10-03 23:04:19 +0200

    Make Bytes::from more generic and implement Debug trait

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,42 @@
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+use std::fmt::{Debug, Error, Formatter};
+
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct Bytes(pub Vec<u8>);
 
-impl From<Vec<u8>> for Bytes {
-    fn from(t: Vec<u8>) -> Self {
-        Bytes(t)
+impl<T> From<T> for Bytes
+where
+    T: AsRef<[u8]>,
+{
+    fn from(t: T) -> Self {
+        Bytes(Vec::from(t.as_ref()))
     }
 }
 
 pub type Key = Bytes;
 pub type Value = Bytes;
+
+impl Debug for Bytes {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Bytes;
+
+    #[test]
+    fn from() {
+        assert_eq!(Bytes::from("foo"), Bytes(vec![102, 111, 111]));
+        assert_eq!(Bytes::from("foo".to_string()), Bytes(vec![102, 111, 111]));
+        assert_eq!(Bytes::from(vec![0x13, 0x37]), Bytes(vec![0x13, 0x37]));
+        assert_eq!(Bytes::from([0x13, 0x37]), Bytes(vec![0x13, 0x37]));
+        assert_eq!(Bytes::from(&[0x13, 0x37]), Bytes(vec![0x13, 0x37]));
+    }
+
+    #[test]
+    fn debug() {
+        assert_eq!(format!("{:?}", &Bytes::from("foo")), "foo");
+        assert_eq!(format!("{:?}", &Bytes::from(&[0x13, 0x37])), "\u{13}7");
+    }
+}
