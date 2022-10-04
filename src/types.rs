44b3d54edccf44454f27d5f085a201f1ab3afac8
Miccah <m.castorina93@gmail.com>
2022-10-04 14:14:54 +0200

    Implement Debug trait for Bytes (#2)

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,6 @@
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+use std::fmt::{Debug, Error, Formatter};
+
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct Bytes(pub Vec<u8>);
 
 impl From<Vec<u8>> for Bytes {
@@ -9,3 +11,25 @@ impl From<Vec<u8>> for Bytes {
 
 pub type Key = Bytes;
 pub type Value = Bytes;
+
+impl Debug for Bytes {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Bytes;
+
+    impl From<&str> for Bytes {
+        fn from(t: &str) -> Self {
+            Bytes(Vec::from(t))
+        }
+    }
+
+    #[test]
+    fn debug_format_is_readable() {
+        assert_eq!(format!("{:?}", &Bytes::from("foo")), "foo");
+    }
+}
